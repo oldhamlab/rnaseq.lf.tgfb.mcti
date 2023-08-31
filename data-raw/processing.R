3fa2fad7e388fd6aee53fb4e3dd6f96c87c9d4f8
William Oldham <wm.oldham@gmail.com>
2023-08-31 16:32:40 -0400

    write Snakefile for salmon index and quant

--- data-raw/processing.R
+++ data-raw/processing.R
@@ -30,4 +30,37 @@ samples <-
     )
   )
 
-# usethis::use_data(processing, overwrite = TRUE)
+
+# merge -------------------------------------------------------------------
+
+files <-
+  list.files(
+    path = "/Volumes/LaCie/rnaseq/rnaseq_lf_tgfb_mcti_BGI_2022/",
+    pattern = "*.fq.gz",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
+fasta_c <- function(files, sample) {
+  pairs <- c("_1.fq.gz", "_2.fq.gz")
+  for (p in pairs) {
+    filename <- stringr::str_c(sample, p)
+    vec <- files[stringr::str_detect(files, sample) & stringr::str_detect(files, p)]
+    system(
+      sprintf(
+        "cat %s > %s",
+        paste(unlist(vec), collapse = " "),
+        stringr::str_c("data-raw/merge/", filename)
+      )
+    )
+  }
+}
+
+dir.create("data-raw/merge")
+purrr::walk(samples$id, \(x) fasta_c(files, x))
+
+
+# salmon ------------------------------------------------------------------
+
+system("snakemake --use-conda -c 1")
+
